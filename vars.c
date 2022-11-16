1db389f9cc5bf9e39fc66c9579b40c34870287b5
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2022-11-16 07:59:53 +0000

    Update vars.c

--- vars.c
+++ vars.c
@@ -6,7 +6,7 @@
  * @buf: the char buffer
  * @p: address of current position in buf
  *
- * Return: 1 if chain delimeter, 0 otherwise
+ * Return: 1 if chain delimeter, 0 otherwise.
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
